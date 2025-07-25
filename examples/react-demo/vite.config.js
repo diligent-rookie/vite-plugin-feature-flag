e72d950907861629a4ff70287c38781014a8672e
diligent-rookie <2337148663@qq.com>
2025-07-25 16:24:59 +0800

    fix: ci workflow

---
+++ examples/react-demo/vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import featureFlagPlugin from 'vite-plugin-feature-flag';
+console.log(featureFlagPlugin);
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), featureFlagPlugin()],
+});
